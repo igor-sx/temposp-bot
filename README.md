72ff8c053b7aa016ae2bc42b160e292792202347
Igor <143670768+lavendercolor@users.noreply.github.com>
2025-04-25 09:38:06 -0300

    obtain full image file URL, with logic for dynamic file name matching

---
+++ README.md
@@ -0,0 +1,5 @@
+#temposp-bot (weather bot)
+This bot periodically fetches and posts an image of the sky from the CGE-SP website, illustrating the dynamic weather and time of day. It also includes a brief summary of the current weather conditions and forecast.
+
+#temposp-bot
+Este bot vai obter e postar, em intervalos regulares, a foto do céu ilustrando o clima e hora do dia dinâmica do site da CGE-SP juntamente com uma informação resumida da previsão do tempo e situação atual.
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 url = 'https://www.cgesp.org/v3//estacoes-meteorologicas.jsp'
- 
 def get_css(url):
     html = requests.get(url)
     bs = BeautifulSoup(html.text, 'html.parser')
-    backgroundImage = bs.find('style').text
-    return backgroundImage
+    backgroundImage = bs.find('style').text #finds the style tag and extracts the full text
+    
+    image_url = None #variable to store the image full url
+    
+    match = re.search(r"\.condTempo\s*\{.*?background.*?:.*?url\((.*?)\)", backgroundImage, re.IGNORECASE | re.DOTALL) #regex to tind the specific file url
+    if match:
+        image_url = match.group(1) #assigns the regex result to image_url var
+        final_url = urljoin(url, image_url) #joins the base url with the image url, providing the full path to the file
+    else:
+        final_url = None
+    return final_url
 
 if __name__ == '__main__':
-    print(f"css: {url}")
-    css2 = get_css(url)
-    print(css2)
+    test = get_css(url)
+    print(f"fullurl: {test}")
