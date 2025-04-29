c3de1792bc1f8d895836e72653df8b09aaa75387
Igor <143670768+lavendercolor@users.noreply.github.com>
2025-04-29 19:30:53 -0300

    refactor: remove disk IO for temp file replacing it with bytes in mem.

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from urllib.parse import urljoin
 import os
 from atproto import Client
 import logging
+from PIL import Image
+import io
+
 #from typing import Optional # will be using | operator instead 
 #from urllib.request import urlretrieve
 #this script scrapes the image file from the weather page and post to bluesky through
@@ -50,53 +53,31 @@ def scrape_image_url(url: str) -> str | None:
         return None
         
 
-def download_image(final_url: str, save_path: str) -> bool:
-    """Save image file to specified path, returns a either True or False if
-    successful or not.
-    
-    Args: 
-        final_url: direct url to image file to be downloaded
-        save_path: path to save the image file
-    Returns: either true or false for success or failure"""
+def download_image_bytes(final_url: str) -> bytes | None:
+    """Download image file from url and return the bytes of the image
+    Args:       final_url: direct url to image file to be downloaded
+    Returns:    bytes of the image file or None if failed."""
     try:
-        save_dir = os.path.dirname(save_path)
-        if save_dir:
-            os.makedirs(save_dir, exist_ok=True)
-        response = requests.get(final_url)
+        response = requests.get(final_url)  
         response.raise_for_status()
-        with open (save_path, 'wb') as f:
-            f.write(response.content)
-        return True
+        content_type = response.headers.get('Content-Type', '')
+        if not content_type.startswith('image/'):
+            logging.error(f"Content not an image: {final_url}, {content_type}")
+            return None
+        logging.info(f"Image downloaded successfully from: {final_url}")
+        return response.content
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error fetching image: {final_url}: {e}")
+        return None
     except Exception as e:
-        logging.error(f"Error downloading image: {e}")
-        return False
-    
-def scrape_and_save(url: str, image_path: str) -> bool:
-    """Orchestrates scraping image URL and downloading the image file
-    to specified path
-    
-    todo: change download_image to download_image_bytes which will return bytes | None 
-    instead of bool and will not save the file.
-    
-    Rename scrape_and_save to something like run_bot_logic
-    new orchestrator function call scrape_image_url
-    If successful, call download_image_bytes; call post_to_bluesky (passing the bytes)
-    
-    might not need to return bool
-    """
-    try:
-        fileurl = scrape_image_url(url)
-        if fileurl:
-            success = download_image(fileurl, image_path)
-            return success
-        return False
-    except Exception as e:
-        logging.error(f"Error scraping image URL: {e}")
-        return False
+        logging.error(f"Unexpected error fetching image: {e}")
+        return None
 
-def post(image_path: str) -> str | None:
+def post(image_data: bytes) -> str | None:
     """Post the image file using atproto lib with os env var credentials, language,
-    and text"""
+    and text.
+    Args:       image_data: bytes of the image file to be posted
+    Returns:    str of the post URI or None if failed."""
     post_uri = None
     if not BLUESKY_PASSWORD or not BLUESKY_USERNAME:
         logging.error("Bluesky credentials not set")
@@ -104,24 +85,43 @@ def post(image_path: str) -> str | None:
     try:
         client = Client()
         client.login(BLUESKY_USERNAME, BLUESKY_PASSWORD)
-        with open(image_path, 'rb') as f:
-            img_data = f.read()
-            response_obj = client.send_image(text="TEST", image=img_data, lang='pt-BR')
-            if hasattr(response_obj, 'uri'):
-                post_uri = response_obj.uri
-                logging.info(f"Image posted successfully: {post_uri}")
-            else:
-                logging.warning(f"Post OK but could not retrieve URI from response")
-                post_uri = None
-    except FileNotFoundError:
-        logging.error(f"File not found at {image_path}")
+        #bytes_data = io.BytesIO(image_data)
+        #bytes_data.name = 'upload.png' #check if it needs a bin stream or can raw like:
+        response_obj = client.send_image(text="test", image=image_data, lang='pt-BR')
+        if hasattr(response_obj, 'uri'):
+            post_uri = response_obj.uri
+            logging.info(f"Image posted successfully: {post_uri}")
+        else:
+            logging.warning(f"Post OK but could not retrieve URI from response")
+            post_uri = None
     except Exception as e:
-        logging.error(f"Error posting image: {e}")
+        logging.error(f"Error submitting post: {e}")
     return post_uri
 
-if __name__ == "__main__":
+def run_bot_logic(url: str) -> str | None:
+    """Orchestrates scraping image URL and downloading image file
+    as bytes, and posting to Bluesky.
+    If successful, call post_to_bluesky (passing the bytes)
+    Args:       url: url to scrape image from
+    Returns:    str of the post URI or None if failed.
+    """
+    logging.info("Starting bot logic...")
+    image_data = None #must initizalize this var?
+    post_uri = None
+    scraped = scrape_image_url(url)
+    if scraped: #if scraped ran successfully, then proceed passing the url ahead
+        image_data = download_image_bytes(scraped)
+        if not image_data: #calls the download function, stores the bytes
+            logging.error("Image download failed, aborting...")
+            return None
+        post_uri = post(image_data) #must inialize this var (?) to call post with the bytes data
+        return post_uri #returns the post uri or None if failed
+    else:
+        logging.error("Scraping failed, aborting...")
+        return None
+
+if __name__ == "__main__": #todo rewrite this entire function to match run_bot_logic
     target_url = 'https://www.cgesp.org/v3//estacoes-meteorologicas.jsp'
-    save_location = os.path.join("tmp", "estacao.png")
     if scrape_and_save(target_url, save_location):
         logging.info("Image downloaded successfully. Starting post...")
         post_result_uri = post(save_location)
